a8cd75ab91e9fab399a7690d5b4ce2cace2b5a26
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-25 17:04:21 -0400

    arrayfilter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string){
+    const newArray = array.filter(name => name.toLowerCase() === string.toLowerCase())
+return newArray
+}
+
+function fuzzyMatch(array, letter){
+    const fuzzyArray = array.filter(name => {
+        return name.charAt(0) === letter.charAt(0)
+    })
+    return fuzzyArray
+}
+    
+
+function matchName (drivers, name){
+    const roadCrew = drivers.filter(drivers => drivers.name === name)
+    return roadCrew
+}
